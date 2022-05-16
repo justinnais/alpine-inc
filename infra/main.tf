6692738fa6e1f63fd3f71f0aa4bd31d470799302
Justin Naismith <justin.n@live.com.au>
2022-05-16 20:11:33 +1000

    Added state file to S3 bucket (#7)

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket            = "rmit-tfstate-3sirfd"
+    key               = "assignment-2/infra-deployment"
+    region            = "us-east-1"
+    dynamodb_table    = "rmit-locktable-3sirfd"
+    dynamodb_endpoint = "https://dynamodb.us-east-1.amazonaws.com"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
